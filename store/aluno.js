487c5e583e983c1be87cf0696b4105af9f05a6c5
Alberto <albertosveloso@gmail.com>
2021-01-11 00:27:47 -0300

    First commit

---
+++ store/aluno.js
@@ -0,0 +1,30 @@
+// aluno.js
+const INCREMENTAR_TEMPO = 'aluno/INCREMENTAR_TEMPO';
+const REDUZIR_TEMPO = 'aluno/REDUZIR_TEMPO';
+const MODIFICAR_EMAIL = 'aluno/MODIFICAR_EMAIL';
+
+export const incrementarTempo = () => ({ type: INCREMENTAR_TEMPO });
+export const reduzirTempo = () => ({ type: REDUZIR_TEMPO });
+export const modificarEmail = (payload) => ({ type: MODIFICAR_EMAIL, payload });
+
+const initialState = {
+  nome: 'Alberto Veloso',
+  email: 'albertosveloso@email.com',
+  diasRestantes: 120,
+};
+
+const reducer = immer.produce((state, action) => {
+  switch (action.type) {
+    case INCREMENTAR_TEMPO:
+      state.diasRestantes++;
+      break;
+    case REDUZIR_TEMPO:
+      state.diasRestantes--;
+      break;
+    case MODIFICAR_EMAIL:
+      state.email = action.payload;
+      break;
+  }
+}, initialState);
+
+export default reducer;
